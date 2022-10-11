057540e3bc0965d5c5eb908411f64dc318a995ce
Kenny Hung (he) <khung@instructors.2u.com>
2022-10-11 21:06:19 +0100

    Test data in code

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-console.log('Hello world')
+function max(first, second) {
+  return 0
+}
+
+// First test
+console.log('Expect: 2')
+console.log(max(1, 2))
+
+// Second test
+console.log('Expect: 6')
+console.log(max(6, -4))
+
+// Third test
+console.log('Expect: 3.4')
+console.log(max(3.4, 2))
 /*
   *
 Input: 1, 2
